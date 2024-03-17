7ce1035a27ebd70423358e21d0060bf150ae44ad
Dave van Rossum <davevanrossum@gmail.com>
2024-03-17 23:53:27 +0100

    Install php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => true,
+        'yoda_style' => true,
+    ])
+    ->setFinder($finder)
+;
